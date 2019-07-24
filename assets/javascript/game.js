cdc1061df46019a08685bed36093f7d3aeb8fe97
MuradUsa <muradusa@Murad.local>
2019-07-23 22:02:05 -0700

    adding variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,21 @@
 
-$(".empty-div").html("<p>Helloooo</p>").append("+ Bye");
+var randomNumnber
+var wins = "0";
+var losses = "0";
+var totalScore;
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+
+var randomNumnber = Math.floor(Math.random()*101) + 19;
+console.log(randomNumnber);
+var one = Math.floor(Math.random()*11) + 1;
+console.log(one);
+
+$(".crystalOne").on("click", function(){
+    var crystalOne = one;
+    console.log(crystalOne);
+}
+)
+$(".totalScore").text(crystalOne);
