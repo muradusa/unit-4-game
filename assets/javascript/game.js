30a728bc1a6124d4eb9bbc111f2a40cbe6365943
MuradUsa <muradusa@Murad.local>
2019-07-25 21:04:50 -0700

    basic jquerry added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,56 @@
 
 var randomNumnber
-var wins = "0";
-var losses = "0";
+
 var totalScore;
 var crystalOne;
 var crystalTwo;
 var crystalThree;
 var crystalFour;
 
-var randomNumnber = Math.floor(Math.random()*101) + 19;
+// Beginning of the game
+
+var wins = 0;
+$(".wins").text(wins);
+
+var losses = 0;
+$(".losses").text(losses);
+
+var randomNumnber = Math.floor(Math.random() * 101) + 19;
 console.log(randomNumnber);
-var one = Math.floor(Math.random()*11) + 1;
+$(".randomNumber").text(randomNumnber);
+
+var one = Math.floor(Math.random() * 11) + 1;
 console.log(one);
 
-$(".crystalOne").on("click", function(){
+var two = Math.floor(Math.random() * 11) + 1;
+console.log(two);
+
+var three = Math.floor(Math.random() * 11) + 1;
+console.log(three);
+
+var four = Math.floor(Math.random() * 11) + 1;
+console.log(four);
+
+// Game is now being played
+
+$(".crystalOne").on("click", function () {
     var crystalOne = one;
     console.log(crystalOne);
-}
-)
-$(".totalScore").text(crystalOne);
+})
+$(".crystalTwo").on("click", function () {
+    var crystalTwo = two;
+    console.log(crystalTwo);
+})
+
+$(".crystalThree").on("click", function () {
+    var crystalThree = three;
+    console.log(crystalThree);
+})
+
+$(".crystalFour").on("click", function () {
+    var crystalFour = four;
+    console.log(crystalFour);
+})
+
+$("#totalScore").text(one);
+
